136feebd6a2df3c9f015ad9c4f11e20fc2658dfd
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-10-06 20:01:36 -0300

    Add objet constructor and create the objet

--- app.js
+++ app.js
@@ -1,46 +1,59 @@
+// Constructor
+function Book(name, autor, pag, isread) {
+    this.name = name;
+    this.autor = autor;
+    this.pag = pag;
+    this.isread = isread;
+}
+
 // Modal Visible
 let addBookBtn = document.querySelector('.addBook');
 let modalContainer = document.querySelector('.modalContainer');
-addBookBtn.addEventListener('click', ()=>{
+addBookBtn.addEventListener('click', () => {
     modalContainer.classList.remove('invisible');
-    modalContainer.classList.add('visible')
+    modalContainer.classList.add('visible');
+});
 
-})
 // Modal Invisible
-let formExit = document.querySelector('.form__exit')
-
-formExit.addEventListener('click', ()=>{
+let formExit = document.querySelector('.form__exit');
+formExit.addEventListener('click', () => {
     modalContainer.classList.remove('visible');
     modalContainer.classList.add('invisible');
-})
+});
 
 // Add Book Functionality
 let form = document.querySelector('.form');
-let inputs = document.querySelectorAll('.form__input')
-let isValidate = false;
-let formError = document.querySelectorAll('.form__error')
+let inputs = document.querySelectorAll('.form__input');
+let formError = document.querySelectorAll('.form__error');
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    for(let errorPara of formError){
-        errorPara.textContent=''
+    let isValidate = true;
+
+    for (let errorPara of formError) {
+        errorPara.textContent = '';
     }
-    for(let input of inputs){
-        if(input.value.trim() === ''){
-            input.nextElementSibling.textContent = 'Empty field !'
-        }
-        else if(input === inputs[0] && Number(input.value)|| input === inputs[1] && Number(input.value) ){
-            input.nextElementSibling.textContent = 'Error the value is not a text !'
-        }
-        else if(input === inputs[2] && isNaN(input.value)){
-            input.nextElementSibling.textContent = 'Error the value is not a number !'
-        }
-        else{
-            isValidate = true;
-            let name = inputs[0].value;
-            let autor = inputs[1].value;
-            let pag = inputs[2].value;
-            let isRead = inputs[3].value;
+
+    for (let input of inputs) {
+        if (input.value.trim() === '') {
+            input.nextElementSibling.textContent = 'Empty field !';
+            isValidate = false;
+        } else if ((input === inputs[0] || input === inputs[1]) && !isNaN(input.value)) {
+            input.nextElementSibling.textContent = 'Error the value is not a text !';
+            isValidate = false; 
+        } else if (input === inputs[2] && isNaN(input.value)) {
+            input.nextElementSibling.textContent = 'Error the value is not a number !';
+            isValidate = false;
         }
     }
-})
+
+    if (isValidate === true) {
+        let name = inputs[0].value;
+        let autor = inputs[1].value;
+        let pag = inputs[2].value;
+        let isRead = inputs[3].checked;
+
+        const book = new Book(name, autor, pag, isRead);
+        console.log(book);
+    }
+});
